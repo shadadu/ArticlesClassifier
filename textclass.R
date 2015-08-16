37d4613df20307e5ff3ef3603f94d6aef00604d7
Shad Adu <sxantwi@gmail.com>
2015-08-16 09:15:40 -0400

    remove ties in knn

--- textclass.R
+++ textclass.R
@@ -2,10 +2,12 @@ textclass <-function(){
   require(tm)
   require(class)
   
+  set.seed(8290)
+   
   wb <- read.table("wb.txt", skip = 0, header=TRUE,  sep="\t", stringsAsFactors=FALSE, quote="")
-  View(wb)
+  
   csvf <- read.table("train.csv", skip = 0, sep="\t", header=TRUE, stringsAsFactors=FALSE, quote="")
-  View(csvf)
+  
     
   csvfFeatures <-csvf$title
   targetData <- csvf$act_code
@@ -17,30 +19,62 @@ textclass <-function(){
   csvfWords <- tm_map(csvfWords, content_transformer(tolower))
   csvfWords <- tm_map(csvfWords, stemDocument, language = "english")
   dtmCsvf<- DocumentTermMatrix(csvfWords)
+   
+  dtmCsvf <-removeSparseTerms(dtmCsvf, 0.99)
   inspect(dtmCsvf)
+  mCsvf <- as.matrix(dtmCsvf)  
+  Terms <- colnames(mCsvf)
   
-  dtmCsvf <-removeSparseTerms(dtmCsvf, 0.9)
-  inspect(dtmCsvf)
-  mCsvf <- as.data.frame(dtmCsvf)
-  trainIndx <- as.integer(0.8*nrow(mCsvf))                   # use 80% of data for training  
-  trainData <- mCsvf[1:trainIndx,]
-  testData <- mCsvf[(trainIndx+1):nrow(mCsvf),]              # and remaining 20% as for testing
+  # Normalize term frequencies
+  normalizer<-function(x){
+    if(max(x)==0){
+          return(1)
+    }else{return(max(x) - min(x))}
+  }
+  
+   
+  
+  dCsvf <- mCsvf
+  for(i in seq_along(Terms)){   
+    dCsvf[,i] <-  mCsvf[,i]/ ( normalizer(mCsvf[,i]) )   
+  }
+ 
+   
+  # To remove ties when knn decides which cluster a point belongs to,
+  # add ~small random (nonbiasing) numbers to the term frequencies
+  perturb <- function(x){
+   if(x==0){
+     x <- 0.0001*runif(1,-1:1) 
+   }else{
+     x <- x*(1 + 0.0001*runif(1,-1:1)) 
+   }
+   x
+ }
+   
+  
+  for(i in seq_along(colnames(dCsvf))){
+    dCsvf[,i] <- sapply(dCsvf[,i], perturb )
+    
+  } 
+ 
+  
+  trainIndx <- as.integer(0.8*nrow(dCsvf))                   
+  trainData <- dCsvf[(1:trainIndx),]
+  testData <- dCsvf[(trainIndx+1):nrow(dCsvf),]              
   trainTarget <-targetData[1:trainIndx]
-  rm(targetData)
-  num <-as.integer(sqrt((nrow(mCsvf))))
+  testTarget <- targetData[(trainIndx+1):nrow(dCsvf)]
   
-  if(num %% 2 == 0)                              # use odd number of nearest neighbors
-    num <- num +1
+  # use odd number of nearest neighbors (k)
+  num <- seq(1,151,10)
+  performance <- c(rep(0,length(num)))
+                                           
+  for(i in seq_along(num)){
+  ml <- knn(train = trainData, test = testData, cl = trainTarget, k = num[i])
+  tbl <- table(testTarget, ml)
+  performance[i] <- 100*sum(diag(tbl))/nrow(testData)
+  }
   
-  #ml <- knn(train = trainData, test = testData, cl = trainTarget, k = num)
-  #table(targetdata, ml)
-  
-  
-  #wbCorpus <- Corpus(VectorSource(wb$title)) 
-  #wbWords <- tm_map(wbCorpus, removeWords, stopwords("english"))
-  #wbWords <- tm_map(wbWords, stripWhitespace)
-  #wbWords <- tm_map(wbWords, content_transformer(tolower))
-  #dtmWb <- DocumentTermMatrix(wbWords)
+  plot(num, performance, type ="l", xlab = "k", ylab ="performance (%)")
   
   
   
